8f6f95ba61a478866601bac101e5b8f1f4db301f
thesondre <e-sonmat1@iskule.no>
2025-03-07 11:25:52 +0100

    ejs

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const cors = require("cors")
+const path = require("path")
+const authRoutes = require("./routes/auth")
+const itemRoutes = require("./routes/items")
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+qpp.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs")
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use("/auth", authRoutes)
+app.use("/items", itemRoutes)
+
+app.get("/", (req, res) => {
+    res.render("index", {user: req.session.user || null});
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
